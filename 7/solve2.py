11462c4470ce1811bb2bf949d0a579a3a7254883
James Gloudemans <james.gloudemans@gmail.com>
2024-12-07 07:50:26 -0500

    day 7

---
+++ 7/solve2.py
@@ -0,0 +1,26 @@
+import itertools
+import operator
+import sys
+
+def cat(left: int, right: int) -> int:
+    return int(''.join((str(left), str(right))))
+
+def main():
+    total = 0
+    for i, line in enumerate(sys.stdin):
+        print(f'{i+1}', end='\r')
+        eq = line.split(':')
+        target = int(eq[0])
+        nums = [int(n) for n in eq[1].split()]
+        for op in map(list, itertools.product([operator.mul, operator.add, cat], repeat=len(nums)-1)):
+            acc = nums[0]
+            for n in nums[1:]:
+                acc = op.pop()(acc, n)
+            if acc == target:
+                total += target
+                break
+    print()
+    print(total)
+
+if __name__ == '__main__':
+    main()
